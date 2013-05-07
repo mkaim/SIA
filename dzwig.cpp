1abdde3d556da8c6a4149ed055ad8fa59cc82290
Lilianna <lila.helminiak@gmial.com>
2013-05-08 01:50:22 +0200

    dwig.cpp is added

---
+++ dzwig.cpp
@@ -0,0 +1,40 @@
+class Dzwig
+{
+	Punkt pozycja;
+	int promienWidzenia;
+	int ListaSasiadow[];
+
+public:
+	Dzwig()
+	{
+		pozycja = new Pozycja(x, y);
+	}
+
+	void dzialaj()
+	{
+		while(statek wyslal zapytanie < x minut temu)
+		{ 
+			Rozejrzyj sie za paczka w swoim zasiegu;
+			if(znalazles)
+			{ }
+			nasluchuj(); //asynchronicznie
+			sciagnijZeStosu();
+			sciagnijZDrogi();
+		}
+	}
+
+	void komunikat(string komunikat, int nadawca)
+	{
+		switch(kom)
+		{
+			case "szukam paczki":
+				break;
+
+			case "usun paczke z drogi":
+				break;
+
+			case "sasiaduje ze statkiem":
+				break;
+		}
+	}
+}
