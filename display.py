84e04383a4b16a822f5ee6b57d4dc3a19aaa8a56
Wojciech Kuprianowicz <wkuprianowicz@gmail.com>
2013-05-16 23:44:56 +0200

    fixed a bug caused by the previous fix

--- display.py
+++ display.py
@@ -41,16 +41,15 @@ class Display:
 	
 	
 	def drawStuff(self, map):
-		craneRectsList = []
+		cranesList = []
 		for row in xrange(map.rowNum):
 			for col in xrange(map.colNum):
 				rect = pygame.draw.rect(self.windowSurface, Display.BLACK, (10 + col * self.fieldSize, 10 + row * self.fieldSize, self.fieldSize, self.fieldSize), 1)
 				
 				if map.fieldType(row, col) == Field.CRANE_TYPE:
 					crane = map.field(row, col).getCrane()
-					
 					craneRect = pygame.draw.circle(self.windowSurface, (100, 100, 100), (rect.centerx, rect.centery), self.fieldSize / 2, 0)
-					craneRectsList.append(craneRect)
+					cranesList.append((crane, craneRect))
 					
 					craneIdText = self.basicFont.render(str(crane.id), True, Display.WHITE, (100, 100, 100))
 					craneIdTextRect = craneIdText.get_rect()
@@ -80,7 +79,8 @@ class Display:
 					crateIdRect.centery = rect.bottom - (self.fieldSize / 4) * i - self.fontSize / 2
 					self.windowSurface.blit(crateId, crateIdRect)
 
-		for craneRect in craneRectsList:
+		for i in xrange(len(cranesList)):
+			(crane, craneRect) = (cranesList[i][0], cranesList[i][1])
 			armLen = sqrt(2) * crane.reach * self.fieldSize
 			pygame.draw.line(self.windowSurface, Display.BLACK, (craneRect.centerx, craneRect.centery), (craneRect.centerx + cos(crane.angle) * armLen, craneRect.centery + sin(crane.angle) * armLen), 3)
 
