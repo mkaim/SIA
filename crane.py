d9264c172bc52ad9231c452cfe2ee67e88a79b05
mkaim <maciejkaim@gmail.com>
2013-05-16 16:56:11 +0200

    Added new crane task: KEEP_BUSY.
    When crane has nothing to do, it's turning around. For now.

--- crane.py
+++ crane.py
@@ -5,8 +5,8 @@ from math import sqrt, atan2, cos, sin, pi
 from time import sleep, time
 
 (SEARCH_PACKAGE, PACKAGE_DELIVERED, HAVE_SHIP_PATH) = range(0, 3)
-(MOVE_ARM, HOOK_UP, HOOK_DOWN, GRAB, DROP) = range(10, 15)
-(TAKE_OFF, PASS_ON) = range(20,22)
+(MOVE_ARM, HOOK_UP, HOOK_DOWN, GRAB, DROP, NOTHING) = range(10, 16)
+(TAKE_OFF, PASS_ON, KEEP_BUSY) = range(20,23)
 
 Message = namedtuple('Message', ['sender', 'type', 'data'])
 
@@ -18,7 +18,7 @@ class Crane:
 		self.rangeSight = rangeSight
 		self.reach = reach
 		self.height = height
-		self.angle = 0
+		self.angle = 0 # in radians, clockwise
 		self.hookDistance = 1
 		self.hookHeight   = height
 		self.neighbours   = neighbours
@@ -31,7 +31,10 @@ class Crane:
 		self.toShip = []
 		self.inWay  = {}
 		self.wanted = {}
-		self.createThread().start()
+
+		self.thread = self.createThread()
+		self.running = True
+		self.thread.start()
 
 	def moveArm(self, alfa, dist):
 		pass
@@ -48,25 +51,31 @@ class Crane:
 	def drop(self):
 		pass
 
+	def doNothing(self):
+		for i in range(0,5):
+			self.angle += 0.1
+			sleep(0.01)
+
+
 	def moveContainer(self, pos1, pos2):
-		def calcAngleAndShift(pos, armAngle):
+		def calcAngleAndShift(pos, armAngle, hookDist):
 			(dx, dy) = (pos[0] - self.position[0], pos[1] - self.position[1])
 			rotate = ((atan2(dy,dx) - armAngle + pi) % (2*pi)) - pi
-			hookShift = sqrt(dy*dy + dx*dx) - self.hookDistance
+			hookShift = sqrt(dy*dy + dx*dx) - hookDist
 			return (rotate, hookShift)
 
-		(rotate1, shift1) = calcAngleAndShift(pos1, self.angle)
-		(rotate2, shift2) = calcAngleAndShift(pos2, self.angle+rotate1)
+		(rotate1, shift1) = calcAngleAndShift(pos1, self.angle, self.hookDistance)
+		(rotate2, shift2) = calcAngleAndShift(pos2, self.angle+rotate1, self.hookDistance+shift1)
 		stack1Size = 1
 		stack2Size = 0
 
 		return [
 			(HOOK_UP, [self.height - self.hookHeight]),
-			(MOVE_ARM, [rotate1, shift1 - self.hookDistance]), 
+			(MOVE_ARM, [rotate1, shift1]), 
 			(HOOK_DOWN, [self.height - stack1Size]),
 			(GRAB, []),
 			(HOOK_UP, [self.height - self.hookHeight]),
-			(MOVE_ARM, [rotate2, shift2 - shift1]), 
+			(MOVE_ARM, [rotate2, shift2]), 
 			(HOOK_DOWN, [self.height - stack2Size]),
 			(DROP, [])
 		]
@@ -82,6 +91,9 @@ class Crane:
 			pass
 		common = getCommonField(craneId)
 		return self.moveContainer(pos, common)
+	
+	def keepBusy(self):
+		return [(NOTHING, [])]
 
 	def informOthers(self):
 		for c in self.neighbours:
@@ -114,6 +126,7 @@ class Crane:
 				self.stopMeasureTime(msg.data.stop)
 		elif msg.type == HAVE_SHIP_PATH:
 			self.toShip.append(msg.sender)
+			self.informOthers()
 
 	def readMessages(self, left=5):
 		while (left > 0 and not self.messages.empty()):
@@ -126,42 +139,46 @@ class Crane:
 			HOOK_UP:   self.hookUp,
 			HOOK_DOWN: self.hookDown,
 			GRAB:      self.grab,
-			DROP:      self.drop
+			DROP:      self.drop,
+			NOTHING:   self.doNothing
 		}.get(inst[0])
 		cmd(*inst[1])
 	
 	def decomposeTask(self, task):
 		dec = {
-			TAKE_OFF: self.takeOff,
-			PASS_ON:  self.passOn,
+			TAKE_OFF:  self.takeOff,
+			PASS_ON:   self.passOn,
+			KEEP_BUSY: self.keepBusy
 		}.get(task[0])
 		return dec(*task[1])
 
-	def isMyArea(self, pos):
+	def isInArea(self, pos):
 		(x, y) = self.position
 		return max(abs(pos[0]-x), abs(pos[1]-y)) <= self.reach
 
 	def doWork(self):
-		if not self.tasks and self.wanted and self.toShip:
-			pass
+		if not self.tasks:
+			if self.wanted and self.toShip:
+				pass
+			else:
+				self.tasks.append((KEEP_BUSY, []))
 
 		if not self.instructions:
-			if not self.tasks:
-				inst = self.doNothing()
-				self.instructions.append(inst)
-			else:
-				task = self.tasks.popleft()
-				inst = self.decomposeTask(task)
-				self.instructions.append(inst)
+			task = self.tasks.popleft()
+			inst = self.decomposeTask(task)
+			self.instructions.extend(inst)
+
 		self.doInst(self.instructions.popleft())
 	
 	def mainLoop(self):
-		while True:
-			self.angle = (self.angle + 1) % 360
+		while self.running:
 			self.examineSurroundings()
 			self.readMessages()
 			self.doWork()
 
 	def createThread(self):
 		return Thread(target=self.mainLoop, args=[])
+
+	def stop(self):
+		self.running = False
 
